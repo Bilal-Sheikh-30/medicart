da80f04ee32e347c8cd37c732e3814535e99ee8c
Bilal-Sheikh-30 <bilalsheikh3011@gmail.com>
2024-10-21 20:21:59 +0500

    added model for symptom and modifies item's model

--- pharma/inventory/models.py
+++ pharma/inventory/models.py
@@ -50,6 +50,9 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     med_formula = models.ForeignKey(MedFormula, on_delete=models.CASCADE, related_name='medFormula', blank=True, null=True)
     category = models.ForeignKey(MedCategory, on_delete=models.CASCADE, related_name='category')
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='itemBy', null=True)
+    # mg of medicine
+    dosage_strength = models.CharField(max_length=50, null=True)
     # form : tablet / syrup / capsules
     form = models.CharField(max_length=50, null=True, blank=True)
     # ml of syp / tabs per strip
@@ -58,8 +61,10 @@ class Item(models.Model):
     min_threshold_qty = models.IntegerField()
     max_threshold_qty = models.IntegerField()
     qty_status = models.CharField(max_length=100)
+    description = models.CharField(max_length=500, null=True, blank=True)
     usage = models.CharField(max_length=1000, blank=True, null=True)
     precautions = models.CharField(max_length=1000, blank=True, null=True)
+    qty_sold = models.IntegerField()
 
     def __str__(self):
         return self.name
@@ -79,3 +84,7 @@ class InventoryOrders(models.Model):
 class MedImage(models.Model):
     itemId = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='itemId')
     image = models.ImageField(upload_to='photos/', blank=True, null=True)
+
+class Symptom(models.Model):
+    med_formula = models.ForeignKey(MedFormula, on_delete=models.CASCADE, related_name='med_formula')
+    symptom = models.CharField(max_length=200, null=True)
