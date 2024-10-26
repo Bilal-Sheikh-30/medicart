f9ea36d0ee6aa74c3bafe2fe6517b6c7f63d9547
Bilal-Sheikh-30 <bilalsheikh3011@gmail.com>
2024-10-26 23:03:43 +0500

    added category and med formulae in inventory.

--- pharma/inventory/urls.py
+++ pharma/inventory/urls.py
@@ -6,11 +6,16 @@ from onlinestore.views import homepage
 app_name = 'inventory'
 
 urlpatterns = [
-    # path('', views.homepage, name='homepage'),
     path('warehouse/', views.warehouse, name='warehousepage'),
     path('sales/', views.sales, name='salespage'),
     path('rider/', views.rider, name='riderpage'),
     path('allcompanies/', views.allcompanies, name='allcompaniespage'),
     path('addcompany/', views.addcompany, name='addcompanypage'),
+    path('viewcategory/', views.viewcategory, name='viewcategorypage'),
+    path('addcategory/', views.addcategory, name='addcategorypage'),
+    path('editcategory/<int:category_id>/', views.editcategory, name='editcategorypage'),
+    path('viewformulae/', views.viewformulae, name='viewformulaepage'),
+    path('addformula/', views.addformula, name='addformulapage'),
+    path('editformula/<int:formula_id>/', views.editformula, name='editformulapage'),
     path('logout/', LogoutView.as_view(next_page=homepage), name='logout'),
 ]
--- pharma/inventory/views.py
+++ pharma/inventory/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from .models import Company
+from .models import Company, MedCategory, MedFormula
 from django.contrib import messages
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse
+
 # Create your views here.
 
 def warehouse(request):
@@ -19,6 +22,60 @@ def addcompany(request):
            Company.objects.create(name=company_name)
            messages.success(request,'added successfully!!')
     return render(request,'addcompany.html')
+
+def viewcategory(request):
+    categories = MedCategory.objects.all()
+    return render(request,'viewcategory.html', {'categories': categories})
+
+def addcategory(request):
+    if request.method == 'POST':
+       category_name = request.POST.get('category_name')
+       if MedCategory.objects.filter(category_name=category_name).exists():
+           messages.error(request,'Category already exists!')
+       else:
+           MedCategory.objects.create(category_name=category_name)
+           messages.success(request,'Category added.')
+    return render(request, 'addcategory.html')
+
+def editcategory(request, category_id):
+    category = get_object_or_404(MedCategory, id=category_id)
+    if request.method == 'POST':
+        new_category_name = request.POST.get('category_name')
+        if MedCategory.objects.filter(category_name=new_category_name).exclude(id=category_id).exists():
+            messages.error(request, 'Category name already exists!')
+        else:
+            category.category_name = new_category_name
+            category.save()
+            messages.success(request, 'Category updated successfully.')
+
+    return render(request, 'addcategory.html', {'category': category})
+
+def viewformulae(request):
+    formulae = MedFormula.objects.all()
+    return render(request,'viewformula.html', {'formulae': formulae})
+
+def addformula(request):
+    if request.method == 'POST':
+       formula_name = request.POST.get('formula_name')
+       if MedFormula.objects.filter(formula_name=formula_name).exists():
+           messages.error(request,'Formula already exists!')
+       else:
+           MedFormula.objects.create(formula_name=formula_name)
+           messages.success(request,'Formula added.')
+    return render(request, 'addformula.html')
+
+def editformula(request, formula_id):
+    formula = get_object_or_404(MedFormula, id=formula_id)
+    if request.method == 'POST':
+        new_formula_name = request.POST.get('formula_name')
+        if MedFormula.objects.filter(formula_name=new_formula_name).exclude(id=formula_id).exists():
+            messages.error(request, 'Formula name already exists!')
+        else:
+            formula.formula_name = new_formula_name
+            formula.save()
+            messages.success(request, 'Formula Name updated successfully.')
+
+    return render(request, 'addformula.html', {'formula': formula})
 
 def sales(request):
     return render(request,'inv_layout.html')
